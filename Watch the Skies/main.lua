05b220ef6d22516e2a3dac9ae69cee897eba33f2
tewlwolow <leonczernecki@gmail.com>
2020-08-27 02:42:23 +0200

    re-added bad weather types

--- Watch the Skies/main.lua
+++ Watch the Skies/main.lua
@@ -10,7 +10,7 @@ local version = "1.0.2"
 local WtC, intWeatherTimer
 
 
-local interiorWeathers={0,1,2,3}
+local interiorWeathers={0,1,2,3,4,5,6,7}
 
 local function debugLog(string)
     if debugLogOn then
