978ed4d81cdf14bade53144bf01a23915d685baa
tewlwolow <leonczernecki@gmail.com>
2020-10-04 14:59:31 +0200

    removed redundancies

--- AURA/Misc/eating.lua
+++ AURA/Misc/eating.lua
@@ -1,27 +1,12 @@
-
 local modversion = require("tew\\AURA\\version")
 local version = modversion.version
 
 local function eating(e)
-
     tes3.getSound("Swallow").volume = 0
 
     if e.item.objectType == tes3.objectType.ingredient then
         tes3.playSound{reference=e.reference, soundPath="Fx\\eating.wav"}
-        timer.start{
-            type=timer.real,
-            duration=1.3,
-            callback=function()
-            tes3.playSound{reference=e.reference, sound="Swallow"}
-        end}
     end
-
-    if e.item.objectType == tes3.objectType.potion then
-        tes3.playSound{soundPath="Fx\\item\\drink.wav"}
-    end
-
-    --tes3.getSound("Swallow").volume = 1.0
-
 end
 
 print("[AURA "..version.."] Eating sound initialised.")
