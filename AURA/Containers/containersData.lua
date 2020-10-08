753669763e5fc51a4ae1003ad5913314bd15690d
tewlwolow <leonczernecki@gmail.com>
2020-10-08 17:54:44 +0200

    added more containers, lock check

--- AURA/Containers/containersData.lua
+++ AURA/Containers/containersData.lua
@@ -1,9 +1,35 @@
 return {
     ["open"]={
+        ["basket"] = "Fx\\RIGHTRN1.wav",
+        ["barrel"] = "Fx\\trans\\drlocked2.wav",
         ["chest"] = "Fx\\chest_opn.wav",
-        ["urn"] = "Fx\\trans\\drstn_opn.wav",
+        ["chest_small"] = "Fx\\DOORM1.wav",
+        ["crate"] = "Fx\\trans\\drmtl_opn.wav",
         ["sack"] = "Fx\\item\\clothes.wav",
-        ["crate"] = "Fx\\trans\\drlocked2.wav",
-
-    }
+        ["urn"] = "Fx\\trans\\drstn_opn.wav",
+        ["desk"] = "Fx\\LEFTRN1.wav",
+        ["closet"] = "Fx\\trans\\drlocked2.wav",
+    },
+    ["close"]={
+        ["basket"] = "Fx\\RIGHTRN2.wav",
+        ["barrel"] = "Fx\\trans\\drlatch_cls2.wav",
+        ["chest"] = "Fx\\chest_cls.wav",
+        ["chest_small"] = "Fx\\DOORM2.wav",
+        ["crate"] = "Fx\\trans\\drmtl_cls.wav",
+        ["sack"] = "Fx\\item\\clothes.wav",
+        ["urn"] = "Fx\\trans\\drstn_cls.wav",
+        ["desk"] = "Fx\\LEFTWK1.wav",
+        ["closet"] = "Fx\\trans\\drlatch_cls2.wav",
+    },
+    ["pitch"]={
+        ["basket"] = 0.7,
+        ["barrel"] = 0.8,
+        ["chest"] = 1.0,
+        ["chest_small"] = 1.5,
+        ["crate"] = 0.8,
+        ["sack"] = 1.5,
+        ["urn"] = 2.5,
+        ["desk"] = 0.5,
+        ["closet"] = 0.6,
+    },
 }
--- AURA/Containers/containersMain.lua
+++ AURA/Containers/containersMain.lua
@@ -12,28 +12,64 @@ local function debugLog(string)
     end
 end
 
-local function playContainerSound(e)
-    if e.target.object.objectType == tes3.objectType.container then
-        tes3.messageBox(e.target.object.id)
+local function getPitch(containerType)
+    if not containerType then return 1.0 end
+    local pitch
+    for k, v in pairs(containersData["pitch"]) do
+        if string.find(containerType:lower(), k) then
+            pitch=v
+        end
+    end
+    return pitch or 1.0
+end
+
+local function playOpenSound(e)
+    if e.target.object.objectType == tes3.objectType.container
+    and not tes3.getLocked({reference=e.target}) then
+
+        local containerType
+
         for type, filepath in pairs(containersData["open"]) do
             if string.find(e.target.object.id:lower(), type) then
                 path=filepath
+                containerType=type
                 break
             end
         end
         if path ~= "" then
-            tes3.playSound{soundPath=path, reference=e.target, volume=0.8, pitch=0.9}
+            tes3.playSound{soundPath=path, reference=e.target, volume=0.8, pitch=getPitch(containerType)}
+            debugLog("Playing container opening sound.")
         end
+        path=""
+
     end
 end
 
+local function playCloseSound(e)
+
+    local containerType
+    for type, filepath in pairs(containersData["close"]) do
+        if string.find(e.reference.object.id:lower(), type) then
+            path=filepath
+            containerType=type
+            break
+        end
+    end
+    if path ~= "" then
+        tes3.playSound{soundPath=path, reference=e.target, volume=0.8, pitch=getPitch(containerType)-0.2}
+        debugLog("Playing container closing sound.")
+    end
+    path=""
+
+end
+
+
 debugLog("Containers module initialised.")
-event.register("activate", playContainerSound)
+
+event.register("activate", playOpenSound)
+event.register("containerClosed", playCloseSound)
 
 --[[ TODO:
-    * clear path after usage
-    * block locked containers
-    * add separate function for closing (uiActivated?)
     * add more types to containersData.lua
     * maybe skip .id, just use .object? General name then shown?
 --]]
