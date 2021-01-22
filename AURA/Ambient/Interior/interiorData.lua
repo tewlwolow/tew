4685482ef45fda47e1630ae9b7b877c164cfec3a
tewlwolow <leonczernecki@gmail.com>
2021-01-22 20:45:19 +0100

    updated data

--- AURA/Ambient/Interior/interiorData.lua
+++ AURA/Ambient/Interior/interiorData.lua
@@ -29,6 +29,11 @@ this.statics = {
 }
 
 this.names = {
+    ["Alchemist"] = {
+        "Alchemist",
+        "Apothecary",
+        "Tel Uvirith, Omavel's House",
+    },
     ["Mages"] = {
         "Mages Guild",
         "Mage's Guild",
@@ -44,11 +49,41 @@ this.names = {
         "Maar Gan Shrine",
         "Vos Chapel",
         "High Fane",
-        "Necrom",
-        "Holamayan"
     },
     ["Library"] = {
         "Library",
+    },
+    ["Trader"] = {
+        "Trader",
+        "Pawnbroker",
+        "Merchandise",
+        "Merchant",
+        "Goods",
+        "Outfitter",
+        "Laborers",
+        "Brewers",
+        "Tradehouse"
+    },
+    ["Clothier"] = {
+        "Clothier",
+        "Tailors",
+    },
+}
+
+this.tavernNames = {
+    ["Dark Elf"] = {
+        --[["Cornerclub",
+        "Corner Club",
+        "Tradehouse",]]
+        "The Rat in the Pot",
+        "House of Earthly Delights",
+    },
+    ["Imperial"] = {
+        "Ebonheart, Six Fishes"
+    },
+    ["Nord"] = {
+        "Skaal Village, The Greathall",
+        "Solstheim, Thirsk"
     }
 }
 
