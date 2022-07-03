94f46df8eab31dc2a4fbe6a83b7c78b3811f7662
tewlwolow <leonczernecki@gmail.com>
2022-07-03 23:45:00 +0200

    Remove redundant user data

--- Vapourmist/main.lua
+++ Vapourmist/main.lua
@@ -5,26 +5,10 @@
 
 local version = require("tew\\Vapourmist\\version")
 local VERSION = version.version
-local data = require("tew\\Vapourmist\\data")
-
-local function loadData()
-    local player = tes3.player
-    if player then
-        player.data.vapourmist = {}
-        player.data.vapourmist.cells = {}
-
-        for _, fogType in pairs(data.fogTypes) do
-            player.data.vapourmist.cells[fogType.name] = {}
-        end
-
-        player.data.vapourmist.cells[data.interiorFog.name] = {}
-    end
-end
 
 local function init()
     mwse.log("[Vapourmist] Version "..VERSION.." initialised.") 
     dofile("Data Files\\MWSE\\mods\\tew\\Vapourmist\\conditionController.lua")
-    event.register("loaded", loadData)
 end
 
 -- Registers MCM menu --
