3134f6b8650958ba5c37f19a80755ed77b5005cf
tewlwolow <leonczernecki@gmail.com>
2022-07-03 12:24:37 +0200

    Shuffled priority a bit

--- AURA/Misc/rainSounds.lua
+++ AURA/Misc/rainSounds.lua
@@ -100,6 +100,6 @@ end
 
 print("[AURA "..version.."] Rain sounds initialised.")
 WtC=tes3.worldController.weatherController
-event.register("weatherChangedImmediate", changeRainSounds, {priority=-180})
-event.register("weatherTransitionImmediate", changeRainSounds, {priority=-180})
-event.register("weatherTransitionStarted", rainStartCheck, {priority=-180})
+event.register("weatherChangedImmediate", changeRainSounds, {priority=-233})
+event.register("weatherTransitionImmediate", changeRainSounds, {priority=-233})
+event.register("weatherTransitionStarted", rainStartCheck, {priority=-233})
