8d71a3d34fc5a63bb81a6e16dad24ca660ba6bf1
tewlwolow <leonczernecki@gmail.com>
2022-07-23 14:04:45 +0200

    Proper UI event register

--- AURA/Misc/waterSplash.lua
+++ AURA/Misc/waterSplash.lua
@@ -6,7 +6,7 @@ local vol = config.splashVol/200
 local function splashPlay(e)
     local element=e.element
     tes3.playSound{soundPath="Fx\\envrn\\splash_lrg.wav", volume=0.5*vol, pitch=0.6}
-    element:register("destroy", function()
+    element:registerAfter("destroy", function()
         tes3.playSound{soundPath="Fx\\envrn\\splash_sml.wav", volume=0.5*vol, pitch=0.8}
     end)
 end
